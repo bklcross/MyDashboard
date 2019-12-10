7de04ed7e57d87b54462aebd1f2f45f1273d5c1e
Anh Dao <addao823@gmail.com>
2019-12-10 11:09:05 -0800

    Setting up necessary pluggins for eslint/prettier

--- schema/User.js
+++ schema/User.js
@@ -1,23 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    task: {
-        type: Schema.Types.ObjectId,
-        ref: "Task"
-    }
-})
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  task: {
+    type: Schema.Types.ObjectId,
+    ref: "Task"
+  }
+});
 
 module.exports = mongoose.model("User", UserSchema);
--- server.js
+++ server.js
@@ -1,50 +1,54 @@
+/* eslint-disable no-console */
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/dashboard", {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb://localhost/dashboard", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const UserDB = require('./schema/User');
+const UserDB = require("./schema/User");
 
 app.post("/user", async (req, res) => {
   const newUserData = req.body;
   try {
-    const newUserDoc = await UserDB.create(newUserData); 
-    res.status(200).json(newUserDoc)
+    const newUserDoc = await UserDB.create(newUserData);
+    res.status(200).json(newUserDoc);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ success: false, error: "Something is wrong"});
+    res.status(500).json({ success: false, error: "Something is wrong" });
   }
-})
+});
 
 //TODO: write route for create task and getting user infomation, with population
 //TODO: write validator for checking login (if req.user exist), for creating user, for creating task
-const TaskDB = require("./schema/Task.js")
+const TaskDB = require("./schema/Task.js");
 
 app.post("/task", async (req, res) => {
   const newTaskData = {
-    user : req.user._id,
-    body : req.body,
-  }
+    user: req.user._id,
+    body: req.body
+  };
 
   try {
     const newTaskDoc = await TaskDB.create(newTaskData);
     res.json(newTaskDoc);
   } catch (err) {
     console.error(err);
-    res.status(500).json({success: false, error: err.message});
+    res.status(500).json({ success: false, error: err.message });
   }
-})
+});
 
 // Send every request to the React app
 // Define any API routes before this runs
